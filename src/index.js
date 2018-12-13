2ca2ca9889af1aa1ae3b89b713e486eb0b6d4789
Nolan Kovacik <noltron000@gmail.com>
2018-12-13 12:54:23 -0800

    🎮 phase 2 - ES6 didn't work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import 'phaser';
 
+function preload() {
+	this.load.image('sky', 'src/assets/sky.png');
+	this.load.image('ground', 'src/assets/platform.png');
+	this.load.image('star', 'src/assets/star.png');
+	this.load.image('bomb', 'src/assets/bomb.png');
+	this.load.spritesheet('dude',
+		'assets/dude.png',
+		{ frameWidth: 32, frameHeight: 48 }
+	);
+}
+
+function create() {
+	this.add.image(400, 300, 'sky');
+	this.add.image(400, 300, 'star');
+}
+
+function update() {
+}
+
 const config = {
 	type: Phaser.AUTO,
 	width: 800,
@@ -12,12 +31,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-const preload = () => {
-}
-
-const create = () => {
-}
-
-const update = () => {
-}
